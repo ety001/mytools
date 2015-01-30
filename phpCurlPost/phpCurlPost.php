2f29b36e0e40f9313096a5d4547dc482b234ed86
ety001 <ety001@domyself.me>
2015-01-31 03:40:47 +0800

    Update phpCurlPost.php

--- phpCurlPost/phpCurlPost.php
+++ phpCurlPost/phpCurlPost.php
@@ -1,10 +1,10 @@
 <?php
-function vpost($url,$data,$cookie){ // 模拟提交数据函数
+function vpost($url,$data,$cookie=''){ // 模拟提交数据函数
     $curl = curl_init(); // 启动一个CURL会话
     curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
-    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在
+    curl_setopt($curl, CURLOPT_USERAGENT, isset($_SERVER['HTTP_USER_AGENT'])?$_SERVER['HTTP_USER_AGENT']:''); // 模拟用户使用的浏览器
     curl_setopt($curl, CURLOPT_COOKIE, $cookie);
     curl_setopt($curl, CURLOPT_REFERER,'https://www.baidu.com');// 设置Referer
     curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
